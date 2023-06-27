49bc6aa7e78bac442073becdd0fc97cde63eba9e
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-06-27 12:49:50 +0200

    add content

---
+++ conf.py
@@ -0,0 +1,44 @@
+# outline for a myst_nb project with sphinx
+# build with: sphinx-build -nW -E --keep-going -b html . ./_build/html
+#  .venv/bin/sphinx-build  -nW -E --keep-going -b html . ./_build/html
+html_favicon = 'favicon.ico'
+
+language = 'en'
+
+# html_static_path = ['_static']
+
+project = 'Manim Share'
+# copyright = ''
+# author = ''
+html_title = "Manim Share"
+
+# specify project details
+master_doc = "index"
+project = "MyST-NB Quickstart"
+
+# basic build settings
+exclude_patterns = ["_build", "README.md"]
+include_patterns = ["*.md", "*.ipynb"]
+
+
+# nb_ipywidgets_js = {
+#     "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js": {
+#         "integrity": "sha256-Ae2Vz/4ePdIu6ZyI/5ZGsYnb+m0JlOmKPjt6XZ9JJkA=",
+#         "crossorigin": "anonymous",
+#     },
+#     "https://cdn.jsdelivr.net/npm/@jupyter-widgets/html-manager@*/dist/embed-amd.js": {
+#         "data-jupyter-widgets-cdn": "https://cdn.jsdelivr.net/npm/",
+#         "crossorigin": "anonymous",
+#     },
+# }
+
+nitpicky = True
+
+nb_execution_mode = "off"
+
+# load extensions
+extensions = ["myst_nb",
+              "sphinx_copybutton"
+]
+
+html_theme = 'furo'
---
+++ index.md
@@ -0,0 +1,21 @@
+# Sharing manim code.
+
+Hello World! 
+
+
+Content:
+
+```{toctree}
+nb_manim_example
+interactive1
+```
+
+Repository on GitHub:  
+<https://github.com/kolibril13/manim_share>
+
+
+
+
+
+
+
